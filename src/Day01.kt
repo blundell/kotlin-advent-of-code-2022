a230572c80efa34e76d5881212cf17eb0393ea36
Paul Blundell <pblundell@twitter.com>
2022-12-01 10:22:15 +0000

    Day 1 part 2 - done

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,14 +1,5 @@
 fun main() {
-    /**
-     * This list represents the Calories of the food carried by five Elves.
-     *
-     * In case the Elves get hungry and need extra snacks, they need to know which Elf to ask:
-     * they'd like to know how many Calories are being carried by the Elf carrying the most Calories.
-     *
-     * Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
-     */
-    fun part1(input: List<String>): Int {
-
+    fun collectElvesCalories(input: List<String>): Map<Int, List<String>> {
         val caloriesByTotal = mutableMapOf<Int, List<String>>()
         var elvesCalories = mutableListOf<String>()
         var runningTotal = 0
@@ -23,11 +14,29 @@ fun main() {
             elvesCalories.add(calories)
         }
 
-        return caloriesByTotal.keys.max()
+        return caloriesByTotal
     }
 
+    /**
+     * This list represents the Calories of the food carried by five Elves.
+     *
+     * In case the Elves get hungry and need extra snacks, they need to know which Elf to ask:
+     * they'd like to know how many Calories are being carried by the Elf carrying the most Calories.
+     *
+     * Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+     */
+    fun part1(input: List<String>): Int {
+        return collectElvesCalories(input).keys.max()
+    }
+
+    /**
+     * Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
+     */
     fun part2(input: List<String>): Int {
-        return input.size
+        return collectElvesCalories(input).keys
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:

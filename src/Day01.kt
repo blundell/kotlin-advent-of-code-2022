76daaa15052d7dd4f28ced8d2a75993f981d6f13
Paul Blundell <blundell.app.dev@gmail.com>
2022-12-01 10:18:25 +0000

    Day 1 done

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,29 @@
 fun main() {
+    /**
+     * This list represents the Calories of the food carried by five Elves.
+     *
+     * In case the Elves get hungry and need extra snacks, they need to know which Elf to ask:
+     * they'd like to know how many Calories are being carried by the Elf carrying the most Calories.
+     *
+     * Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+     */
     fun part1(input: List<String>): Int {
-        return input.size
+
+        val caloriesByTotal = mutableMapOf<Int, List<String>>()
+        var elvesCalories = mutableListOf<String>()
+        var runningTotal = 0
+        for (calories in input) {
+            if (calories.isBlank()) {
+                caloriesByTotal[runningTotal] = elvesCalories
+                elvesCalories = mutableListOf()
+                runningTotal = 0
+                continue
+            }
+            runningTotal += calories.toInt()
+            elvesCalories.add(calories)
+        }
+
+        return caloriesByTotal.keys.max()
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +32,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 1000)
 
     val input = readInput("Day01")
     println(part1(input))
